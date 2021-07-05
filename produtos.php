54cb3b85bd3a1482868375276f4f441edd143fda
Natália Nascimento <natalia.nascc@gmail.com>
2021-07-05 17:49:39 -0300

    Estrutura inicial do site da Barbearia!

---
+++ produtos.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Produtos - Barbearia Alura</title>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/produtos.css">
+		<link rel="stylesheet" href="css/footer.css">
+	</head>
+	<body>
+	
+		<?php require_once('header.html');?>
+		
+		<main><!-- CONTEÚDO PRINCIPAL -->
+			<ul class="produtos">
+				<li>
+					<h2>Cabelo</h2>
+					<img alt="" src="imagens/cabelo.png">
+					<p class="produto-descricao">Na tesoura ou máquina, como o cliente preferir.</p>
+					<p class="produto-preco">R$ 25,00</p>
+				</li>
+				
+				<li>
+					<h2>Barba</h2>
+					<img alt="" src="imagens/barba.png">
+					<p class="produto-descricao">Corte e desenho profissional de barba.</p>
+					<p class="produto-preco">R$ 18,00</p>
+				</li>
+				
+				<li>
+					<h2>Cabelo & Barba</h2>
+					<img alt="" src="imagens/cabelo+barba.png">
+					<p class="produto-descricao">Pacote completo de cabelo e barba.</p>
+					<p class="produto-preco">R$ 35,00</p>
+				</li>
+			</ul>
+		</main>
+		
+		<?php require_once('footer.html');?>
+		
+	</body>
+</html>
